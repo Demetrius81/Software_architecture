c0839bc1a9989d5110efc85d88a7792b7bf73970
Dmitri <shizimaniak@gmail.com>
2022-11-25 13:09:56 +0300

    реализована прослойка менеджера

---
+++ Lesson8/RestaurantService/RestaurantService/Services/IOrderManager.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantService.Models;
+
+namespace RestaurantService.Services;
+
+public interface IOrderManager
+{
+    Task<IEnumerable<Order>> GetAllOrdersAsync(CancellationToken cancel = default);
+    Task<Order?> GetOrderByIdAsync(int id, CancellationToken cancel = default);
+    Task<int> GetCountOrdersAsync(CancellationToken cancel = default);
+    Task<int> AddOrderAsync(Order item, CancellationToken cancel = default);
+    Task<bool> UpdateOrderAsync(Order item, CancellationToken cancel = default);
+    Task<bool> DeleteOrderAsync(Order item, CancellationToken cancel = default);
+}
---
+++ Lesson8/RestaurantService/RestaurantService/Services/OrderManager.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantService.Models;
+using RestaurantService.Services.Repository;
+
+namespace RestaurantService.Services;
+
+public class OrderManager : IOrderManager
+{
+    private readonly IRepositoryAsync<Order> _repository;
+    private readonly ILogger<OrderManager> _logger;
+
+    public OrderManager(IRepositoryAsync<Order> repository, ILogger<OrderManager> logger)
+    {
+        this._repository = repository;
+        this._logger = logger;
+    }
+    public async Task<int> AddOrderAsync(Order item, CancellationToken cancel = default)
+    {
+        return await _repository.AddAsync(item, cancel);
+    }
+
+    public async Task<bool> DeleteOrderAsync(Order item, CancellationToken cancel = default)
+    {
+        return await _repository.DeleteAsync(item, cancel);
+    }
+
+    public async Task<IEnumerable<Order>> GetAllOrdersAsync(CancellationToken cancel = default)
+    {
+        return await _repository.GetAllAsync(cancel);
+    }
+
+    public async Task<int> GetCountOrdersAsync(CancellationToken cancel = default)
+    {
+        return await _repository.GetCountAsync(cancel);
+    }
+
+    public async Task<Order?> GetOrderByIdAsync(int id, CancellationToken cancel = default)
+    {
+        return await _repository.GetByIdAsync(id, cancel);
+    }
+
+    public async Task<bool> UpdateOrderAsync(Order item, CancellationToken cancel = default)
+    {
+        return await _repository.UpdateAsync(item, cancel);
+    }
+}
