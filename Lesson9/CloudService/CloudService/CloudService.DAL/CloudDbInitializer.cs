86cdd3bd5a6cceff94d8b46a8ed1189156950489
Dmitri <Shizimaniak@gmail.com>
2022-12-01 19:15:06 +0300

    Добавлен инициализатор БД

--- Lesson9/CloudService/CloudService/CloudService.DAL/CloudDbInitializer.cs
+++ Lesson9/CloudService/CloudService/CloudService.DAL/CloudDbInitializer.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +16,7 @@ public class CloudDbInitializer
     private readonly ILogger<CloudDbInitializer> _logger;
 
     public CloudDbInitializer(CloudServiceDb db, ILogger<CloudDbInitializer> logger)
-	{
+    {
         _db = db;
         _logger = logger;
     }
@@ -41,26 +42,81 @@ public class CloudDbInitializer
 
         var rnd = new Random();
 
-        var clients = Enumerable.Range(1, 10)
-            .Select(x => new Client
-            { 
-                FirstName = $"FirstName-{x}",
-                LastName = $"LastName-{x}",
-                Patronymic = $"Patronymic-{x}",
-                Email = $"Email-{x}",
-                PasswordHash = $"PasswordHash{x}"
-            });
+        if (!await _db.Clients.AnyAsync(cancel))
+        {
+            var clients = Enumerable.Range(1, 10)
+                .Select(x => new Client
+                {
+                    FirstName = $"FirstName-{x}",
+                    LastName = $"LastName-{x}",
+                    Patronymic = $"Patronymic-{x}",
+                    Email = $"Email-{x}",
+                    PasswordHash = $"PasswordHash{x}"
+                });
 
-        var ipAddresses = Enumerable.Range(1, 254)
-            .Select(x => new IpAddress
-            {
-                Value = x
-            });
+            await _db.Clients.AddRangeAsync(clients, cancel);
+            _logger.LogInformation(">>> Таблица клиентов добавлена");
+            await _db.SaveChangesAsync(cancel);
+        }
 
-        var servers = Enumerable.Range(1, 20)
-            .Select(x => new Server
-            {
+        if (!await _db.IpAddresses.AnyAsync(cancel))
+        {
+            var ipAddresses = Enumerable.Range(1, 254)
+                .Select(x => new IpAddress
+                {
+                    Value = x
+                });
 
-            })
+            await _db.IpAddresses.AddRangeAsync(ipAddresses, cancel);
+            _logger.LogInformation(">>> Таблица IP адресов добавлена");
+            await _db.SaveChangesAsync(cancel);
+        }
+        if (!await _db.Servers.AnyAsync(cancel))
+        {
+            var servers = Enumerable.Range(1, 20)
+                .Select(x => new Server
+                {
+                    Ram = rnd.Next(16, 128),
+                    Rom = rnd.Next(1, 10),
+                    Cpu = rnd.Next(4, 28),
+                    Os = OperationSystem.Debian
+                });
+
+            await _db.Servers.AddRangeAsync(servers, cancel);
+            _logger.LogInformation(">>> Таблица серверов добавлена");
+            await _db.SaveChangesAsync(cancel);
+        }
+
+        if (!await _db.ServerPools.AnyAsync(cancel))
+        {
+            var serverPools = Enumerable.Range(1, 20)
+                .Select(x => new ServerPool
+                {
+                    ServerId = x
+                });
+
+            await _db.ServerPools.AddRangeAsync(serverPools, cancel);
+            _logger.LogInformation(">>> Таблица пул серверов добавлена");
+            await _db.SaveChangesAsync(cancel);
+        }
+
+        if (!await _db.Clouds.AnyAsync(cancel))
+        {
+            var clouds = Enumerable.Range(1, 5)
+                .Select(x => new Cloud
+                {
+                    ClientId = rnd.Next(1, 10),
+                    IpAddressId = rnd.Next(1, 254),
+                    ServerPoolId = rnd.Next(1, 20),
+                    Ram = rnd.Next(16, 128),
+                    Rom = rnd.Next(1, 10),
+                    Cpu = rnd.Next(4, 28),
+                    Os = OperationSystem.Debian
+                });
+
+            await _db.Clouds.AddRangeAsync(clouds, cancel);
+            _logger.LogInformation(">>> Таблица облако добавлена");
+            await _db.SaveChangesAsync(cancel);
+        }
     }
 }
